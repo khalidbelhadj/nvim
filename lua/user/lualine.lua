1df7069e8fb450d344db5bf89a346857648e176a
Khalid Belhadj <khalidbelhadj@Khalids-MBP.lan>
2022-07-28 03:41:16 +0400

    Set configuration to lua file

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -2,9 +2,9 @@ require('lualine').setup {
     options = {
         icons_enabled = true,
         theme = 'auto',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
-        disabled_filetypes = {'NvimTree'},
+        component_separators = '',
+        section_separators = '',
+        disabled_filetypes = {'NvimTree', 'alpha'},
         always_divide_middle = true,
         globalstatus = false,
         refresh = {
@@ -17,7 +17,7 @@ require('lualine').setup {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
         lualine_c = { 'filename' },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_x = { 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' }
     },
@@ -29,8 +29,4 @@ require('lualine').setup {
         lualine_y = {},
         lualine_z = {}
     },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = {}
 }
